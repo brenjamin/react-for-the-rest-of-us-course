1febb896b9a18b76d7e30d09f8ad7ac28bff5787
Ben <Ben@Bens-MacBook-Pro.local>
2021-12-28 12:57:52 -0500

    Login form validation

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,37 +1,83 @@
 import React, { useEffect, useState, useContext } from "react"
+import { useImmerReducer } from "use-immer"
 import Axios from "axios"
 import DispatchContext from "../DispatchContext"
 
 function HeaderLoggedOut(props) {
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
+  const initialState = {
+    username: {
+      value: "",
+      hasErrors: false
+    },
+    password: {
+      value: "",
+      hasErrors: false
+    },
+    submitCount: 0
+  }
 
-  const appDispatch = useContext(DispatchContext)
-
-  async function handleSubmit(e) {
-    e.preventDefault(e)
-    try {
-      const response = await Axios.post("/login", { username, password })
-      if (response.data) {
-        appDispatch({ type: "login", data: response.data })
-        appDispatch({ type: "flashMessage", value: "You have successfully logged in." })
-      } else {
-        console.log("Incorrect username/password")
-        appDispatch({ type: "flashMessage", value: "Invalid username/password" })
-      }
-    } catch (e) {
-      console.log("There was a problem")
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "validateUsername":
+        draft.username.hasErrors = false
+        draft.username.value = action.value
+        if (!draft.username.value) {
+          draft.username.hasErrors = true
+        }
+        return
+      case "validatePassword":
+        draft.password.hasErrors = false
+        draft.password.value = action.value
+        if (!draft.password.value) {
+          draft.password.hasErrors = true
+        }
+        return
+      case "submitForm":
+        if (!draft.username.hasErrors && !draft.password.hasErrors) {
+          draft.submitCount++
+        }
+        return
     }
   }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+  const appDispatch = useContext(DispatchContext)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: "validateUsername", value: state.username.value })
+    dispatch({ type: "validatePassword", value: state.password.value })
+    dispatch({ type: "submitForm" })
+  }
+
+  useEffect(() => {
+    if (state.submitCount) {
+      async function submitForm() {
+        try {
+          const response = await Axios.post("/login", { username: state.username.value, password: state.password.value })
+          if (response.data) {
+            appDispatch({ type: "login", data: response.data })
+            appDispatch({ type: "flashMessage", value: "You have successfully logged in." })
+          } else {
+            console.log("Incorrect username/password")
+            appDispatch({ type: "flashMessage", value: "Invalid username/password" })
+          }
+        } catch (e) {
+          console.log("There was a problem")
+        }
+      }
+      submitForm()
+    }
+  }, [state.submitCount])
 
   return (
     <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={e => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+          <input onChange={e => dispatch({ type: "validateUsername", value: e.target.value })} name="username" className={"form-control form-control-sm input-dark " + (state.username.hasErrors ? "is-invalid" : "")} type="text" placeholder="Username" autoComplete="off" />
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={e => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+          <input onChange={e => dispatch({ type: "validatePassword", value: e.target.value })} name="password" className={"form-control form-control-sm input-dark " + (state.password.hasErrors ? "is-invalid" : "")} type="password" placeholder="Password" />
         </div>
         <div className="col-md-auto">
           <button className="btn btn-success btn-sm">Sign In</button>
